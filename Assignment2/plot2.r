8b0be55645f8d42a0e7661e2b092e90edf391cbc
rahulsunder88 <rahulsunder88@gmail.com>
2016-01-25 02:08:31 +0530

    Week1 assignment
    Week1 assignment

---
+++ Assignment2/plot2.r
@@ -0,0 +1,8 @@
+data1 <- read.table("c:/Users/Durga/Desktop/household_power_consumption.txt",header = TRUE,sep = ";", stringsAsFactors = FALSE)
+data1$Date <- as.Date(data1$Date,"%d/%m/%Y")
+data3 <- subset.data.frame(data1,data1$Date>= as.Date("2007-02-01") & data1$Date <= as.Date("2007-02-02"))
+datetime <- strptime(paste(data3$Date,data3$Time), "%Y-%m-%d %H:%M:%S") 
+data3$Global_active_power <- as.numeric(data3$Global_active_power)
+png("plot2.png",width=480,height=480)
+plot(datetime,data3$Global_active_power,type = 'l', xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
